c529097866514a066ff133d96df6aef70fc41ef7
thankgod <ugothankgod729@gmail.com>
2023-09-18 08:36:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var App = function App() {
+  return React.createElement(
+    Template,
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Main content'
+    )
+  );
+};
+var container = document.getElementById('root');
+var root = ReactDOM.createRoot(container);
+root.render(React.createElement(App, null));
